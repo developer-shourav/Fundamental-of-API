37f887f537e9d4790c8105b977bbceb74b14781e
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-01-13 16:56:47 +0600

    object example

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("To get something you never had, you have to do something you never did.");
-console.log("This was last console log");
+/* console.log("To get something you never had, you have to do something you never did.");
+console.log("This was last console log"); */
+
+user = {id:01, userName:"Shourav", job:"Web developments"} ;
+
+console.log(user);
